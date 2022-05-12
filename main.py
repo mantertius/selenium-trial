e062423650539d1eb9504911b4185920a86e2500
mantertius <mpat@ic.ufal.br>
2022-05-12 18:07:30 -0300

    Completes the adding cycle

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.common.exceptions import StaleElementReferenceException
+from selenium.common.exceptions import StaleElementReferenceException, InvalidArgumentException
 from decouple import config
 import re
 import os
@@ -51,8 +51,8 @@ def drag_and_drop_file(drop_target, path):
     driver = drop_target.parent
     file_input = driver.execute_script(JS_DROP_FILE, drop_target, 0, 0)
     file_input.send_keys(path)
-
-
+    print(f'Consegui fazer o upload de {path}')
+    return True
 
 def login_proradis() -> webdriver.Chrome:
     """
@@ -136,22 +136,22 @@ def send_Electro(driver:webdriver.Chrome) -> webdriver.Chrome:
     ecgRadial = driver.find_element(By.XPATH,'//*[@id="mod-ECG"]').click()
     dateRangeFinder = driver.find_element(By.XPATH,'//*[@id="entre"]').click()
     
-    date = input('Coloque a data formatada [dd/mm/aaaa]:')
+    date = '02/05/2022' #input('Coloque a data formatada [dd/mm/aaaa]:')
    
     dateInit = driver.find_element(By.XPATH,'//*[@id="data_inicio"]').send_keys(date)
-    dateEnd = driver.find_element(By.XPATH,'//*[@id="data_fim"]').send_keys(date + ENTER)
+    dateEnd = driver.find_element(By.XPATH,'//*[@id="data_fim"]').send_keys(date)
     date = date.strip().split('/')
     day = date[0]
     month = date[1]
     year = date[2]
 
-    breakpoint()
 
     submitBtn = driver.find_element(By.XPATH,'//*[@id="full_search"]').send_keys(ENTER)
     driver.implicitly_wait(20)
-    WebDriverWait(driver,3,5,(StaleElementReferenceException)).until(lambda d:driver.find_element(By.XPATH,'//*[@id="vApp"]/div[5]/div').is_displayed())
+    WebDriverWait(driver,3,10,(StaleElementReferenceException)).until(lambda d:driver.find_element(By.XPATH,'//*[@id="vApp"]/div[5]/div').is_displayed())
     
     originalWindow = driver.current_window_handle
+    #breakpoint()
     weirdClass  = driver.find_element(By.CSS_SELECTOR,".report-line.odd")
     driver.execute_script("arguments[0].click();",weirdClass)
 
@@ -160,10 +160,11 @@ def send_Electro(driver:webdriver.Chrome) -> webdriver.Chrome:
         if window_handle != originalWindow:
             driver.switch_to.window(window_handle)
             print("Window Changed!")
-            break
+            break;
 
-    WebDriverWait(driver,5,5).until(lambda d: driver.find_element(By.XPATH,'//*[@id="report-editor"]').is_displayed())
+    WebDriverWait(driver,3).until(lambda d: driver.find_element(By.XPATH,'//*[@id="simplemodal-overlay"]').is_displayed())
     neglectResponsible = driver.find_element(By.XPATH,'//*[@id="simplemodal-overlay"]').click()
+    print('neglected')
     innerHTML = driver.find_element(By.ID,"left-panel").get_attribute('innerHTML')
     result = re.search(re.escape('PACS: ')+"(.*)"+re.escape('</small></span> <br><span><small>'), innerHTML)
     patName = result[0].split('</small>')[0].split('PACS: ')[1].strip()
@@ -174,9 +175,23 @@ def send_Electro(driver:webdriver.Chrome) -> webdriver.Chrome:
     #breakpoint()
     initPath = r'\\172.19.0.2\\exames-eletro' #r'\\172.19.0.2\exames-eletro\2022\04 - Abril\27'
     
-    path = initPath+f'\\{year}\\{get_Month_Full_Path(month)}'
-    drag_and_drop_file(dropzone, initPath+patName)
-    
+    path = initPath+f'\\{year}\\{get_Month_Full_Path(month)}\\{day}\\'
+    fullPath = path + patName+'.jpg'
+    try:
+        #upload = drag_and_drop_file(dropzone, fullPath)
+        breakpoint()
+        leaveDropzone = driver.find_element(By.XPATH,'//*[@id="simplemodal-container"]/a').click()
+        addImage = driver.find_element(By.XPATH,'/html/body/div[2]/div[2]/div/div[2]/div[3]/div/div[2]/button[21]').click() 
+        upload = drag_and_drop_file(dropzone, fullPath)
+        leaveDropzone = driver.find_element(By.XPATH,'//*[@id="simplemodal-container"]/a').click()
+        print(f'Paciente {patName} finalizado. Indo para o próximo paciente.')
+        if not driver.find_element(By.XPATH,'//*[@id="next"]').is_enabled():
+            print('Lista de pacientes finalizada.')
+        driver.find_element(By.XPATH,'//*[@id="next"]').click()
+        send_Electro(driver)   
+
+    except InvalidArgumentException:
+        print(f'Paciente {patName} não encontrado')
 
 
 driver = login_proradis()
